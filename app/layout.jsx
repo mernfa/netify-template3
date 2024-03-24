2d4b39af097b1a3556a10babc4fd456931e301b9
mernfa <138582701+mernfa@users.noreply.github.com>
2024-03-24 10:53:52 +0200

    adding header to project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,11 @@
 import '../styles/globals.css';
-
+import Header from '@/components/header';
 
 export default function RootLayout({ children }) {
    return (
       <html lang="en">
-         <body className=' bg-white text-black dark:bg-zinc-800 '>
-            {children}
+         <body className=' p-4 flex flex-col gap-8 bg-white text-black dark:bg-zinc-800 '>
+            <Header/> {children}
          </body>
       </html>
    )
---
+++ components/header/index.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+   return (
+      <div className=" bg-blue-600 text-white p-6 rounded-xl">
+         header is this
+      </div>
+   );
+}
+
+export default Header;
